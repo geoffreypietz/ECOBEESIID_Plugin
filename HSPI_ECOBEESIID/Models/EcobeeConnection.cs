c85f169e1efe257211789390b13204cd2ef66370
newneonova <botline@gmail.com>
2019-08-13 10:29:24 -0600

    3.0.3.11 added more descriptive errorr logging

--- HSPI_ECOBEESIID/Models/EcobeeConnection.cs
+++ HSPI_ECOBEESIID/Models/EcobeeConnection.cs
@@ -112,15 +112,84 @@ namespace HSPI_Ecobee_Thermostat_Plugin.Models
                 }
             }
 
+            //only get here if we don't make it to return. So the access_token is missing. or the JSON did deserialize the object as indended
             if (200 <= ((int)response.StatusCode) && ((int)response.StatusCode) < 400) {
-                Util.Log(response.StatusDescription, Util.LogType.LOG_TYPE_ERROR);
+
+                string message = "";
+                if (response.ErrorMessage != null)
+                {
+                    message = response.ErrorMessage;
+                }
+
+                switch ((int)response.StatusCode){
+
+                    case 0:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode+" "+ message + " the request was successful but the access token was not parsed from the response correctly", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+
+                    case 1:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Authentication failed. Invalid credentials supplied to the registration request, or invalid token. Request registration again.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 2:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Not authorized. Attempted to access resources which user is not authorized for. Ensure the thermostat identifiers requested are correct.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 3:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Processing error. General catch-all error for a number of internal errors. Additional info may be provided in the message. Check your request. Contact support if persists.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 4:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Serialization error. An internal error mapping data to or from the API transmission format. Contact support.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 5:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Invalid request format. An error mapping the request data to internal data objects. Ensure that the properties being sent match properties in the specification.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 6:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Too many thermostat in selection match criteria. Too many identifiers are specified in the Selecton.selectionMatch property. Current limit is 25 per request.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 7:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Validation error. The update request contained values out of range or too large for the field being updated. See the additional message information as to what caused the validation failure.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 8:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Invalid function. The \"type\" property of the function does not match an available function. Check your request parameters.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 9:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Invalid selection. The Selection.selectionType property contains an invalid value.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 10:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Invalid page. The page requested in the request is invalid. Occurs if the page is less than 1 or more than the number of available pages for the request.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 11:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Function error. An error occurred processing a function. Ensure required properties are provided.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 12:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Post not supported for request. The request URL does not support POST.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 13:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Get not supported for request. The request URL does not support GET.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 14:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + "	Authentication token has expired. Refresh your tokens. ", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 15:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + " Duplicate data violation.", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    case 16:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode + " " + message + "	Invalid token. Token has been deauthorized by user. You must re-request authorization. ", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                    default:
+                        Util.Log(response.StatusDescription + " " + response.StatusCode, Util.LogType.LOG_TYPE_ERROR);
+                        Util.Log("Invalid Refresh Token-Try resetting the token on the Options Page", Util.LogType.LOG_TYPE_ERROR);
+                        break;
+                }
+
+
+               
 
             }
             else {
                 Util.Log(response.StatusDescription+"  "+response.ErrorMessage, Util.LogType.LOG_TYPE_ERROR);
             } 
           
-            Util.Log("Invalid Refresh Token-Try resetting the token on the Options Page", Util.LogType.LOG_TYPE_ERROR);
+          
             return false;
 
 
